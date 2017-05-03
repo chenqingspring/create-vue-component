5bc7540fec67420a72f062432d7c424d2e9982f8
CHEN Qing <chenqing@sevencent.com.cn>
2017-05-03 11:21:45 +0800

    fix dist path by env

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
+    publicPath: process.env.NODE_ENV === 'production' ? './dist/' : '/dist/',
     filename: 'build.js'
   },
   module: {
